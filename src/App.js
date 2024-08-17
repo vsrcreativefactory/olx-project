09ce5d2fa81b3cd8b5295f277bee795af2ed72b4
vsrcreativefactory <141257315+vsrcreativefactory@users.noreply.github.com>
2024-08-17 21:37:21 +0530

    Signup setup made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect,useContext} from 'react';
 import './App.css';
 import { BrowserRouter as Router ,Route } from 'react-router-dom/cjs/react-router-dom';
 import Signup from './Pages/Signup'
+import Login from './Pages/Login'
 import './App.css'
-
+import { AuthContext, FirebaseContext } from './store/FirebaseContext'
 
 /**
  * ?  =====Import Components=====
@@ -11,6 +12,13 @@ import './App.css'
 import Home from './Pages/Home';
 
 function App() {
+  const {setUser} = useContext(AuthContext)
+  const {firebase} = useContext(FirebaseContext)
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+      setUser(user)
+    })
+  })
   return (
     <div>
       <Router>
@@ -19,6 +27,9 @@ function App() {
         </Route>
         <Route path="/signup">
         <Signup />
+        </Route>
+        <Route path="/login">
+        <Login />
         </Route>
       </Router>
       
